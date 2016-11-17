1888bb3fe4ab92172c3e9d6925dcf0a9b715b36c
Artem Radchenko <art.radchenko@gmail.com>
2016-11-17 20:38:11 +0000

    Added mailer and view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,7 +5,18 @@ class ContactsController < ApplicationController
   
   def create
      @contact = Contact.new(contact_params)
+     
     if @contact.save
+      
+      # recorded variables to send the email to myself based on template in view/contact_mailer/contact_email.html.erb
+      name = params[:contact][:name] #nested hash, name, email, comments are inside contact. Rails names this entire hash 'params'.
+      email = params[:contact][:email]
+      body = params[:contact][:comments]
+      
+      # run ContactMailer from contact_mailer.rb
+      ContactMailer.contact_email(name, email, body).deliver
+      
+      # show success message
       flash[:success] = 'Message sent.'
       redirect_to new_contact_path
     else
