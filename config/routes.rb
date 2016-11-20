007e607238eba736bdaeaf5bd584ccacff9491bd
Artem Radchenko <art.radchenko@gmail.com>
2016-11-20 22:05:10 +0000

    Added profiles new action, fixed profile controller folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
   
   devise_for :users, controllers: { registrations: 'users/registrations' }
-
   resources :users do
     resource :profile
   end
-
-
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   resources :contacts, only: :create
