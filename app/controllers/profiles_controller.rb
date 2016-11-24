cabd986ae96ddfd0557c6d6558b437801b21946e
Artem Radchenko <art.radchenko@gmail.com>
2016-11-24 21:25:01 +0000

    Added before filters to check correct user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,9 @@
 class ProfilesController < ApplicationController
   
+  #before_action it's mean that its run before any of next actions (before new, before create...)
+  before_action :authenticate_user! #devise gem thing to protect from changing profile by un-authenticate users
+  before_action :only_current_user #devise gem thing to protect from cross-users profile editing, additional we difine it in private section below
+  
   def new
     # form where a user can fill out their own profile.
     @user = User.find( params[:user_id] )
@@ -8,7 +12,7 @@ class ProfilesController < ApplicationController
   
   def create 
     @user = User.find( params[:user_id] )
-    @profile = @user.build_profile(profile_params)
+    @profile = @user.build_profile(profile_params) #create profile via gem devise
     if @profile.save
       flash[:success] = "Profile Updated!"
       redirect_to user_path( params[:user_id] )
@@ -22,8 +26,25 @@ class ProfilesController < ApplicationController
     @profile = @user.profile
   end
   
+  def update
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile #open current user's profile
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile Updated!"
+      redirect_to user_path( params[:user_id] ) # redirecting to users own page
+    else
+      render action: :edit #go back to edit page
+    end
+  end
+  
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
     end
+    
+    def only_current_user
+      @user = User.find( params[:user_id] )
+      redirect_to(root_url) unless @user == current_user
+    end
+    
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  
+  before_action :authenticate_user! #devise gem thing to protect from un-authenticate users
+  
   def show
     @user = User.find( params[:id] )
   end
