4d4a9c7b92398c367bf1371584ee535fb6ccd617
Artem Radchenko <art.radchenko@gmail.com>
2016-11-29 20:45:38 +0000

    Improved users index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user! #devise gem thing to protect from un-authenticate users
   
   def index
+   @users = User.includes(:profile)
   end
   
   def show
