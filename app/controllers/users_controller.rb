5d45c4baaea855e81230cfc346e4f990dfa2bf0a
Artem Radchenko <art.radchenko@gmail.com>
2016-11-29 19:48:22 +0000

    Added index action and updated link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
   
   before_action :authenticate_user! #devise gem thing to protect from un-authenticate users
   
+  def index
+  end
+  
   def show
     @user = User.find( params[:id] )
   end
